3127f95994df9651ffbc4fcd2e5a8999e2c95756
Oleksandr Fatikov <portasanych@gmail.com>
2020-04-06 09:42:42 +0000

    rearrange apps

---
+++ complex/client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Link } from 'react-route-dom';
+
+export default () => {
+	return (
+		<div>
+		In some other Page!
+		</div>
+		<Link to="/"> Go back home </Link>
+		</div>
+		);
+	};
