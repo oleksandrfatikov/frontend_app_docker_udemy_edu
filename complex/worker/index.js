d5379d880db885569cd49007f9b419f0dbdc3547
Oleksandr Fatikov <portasanych@gmail.com>
2020-04-13 10:16:21 +0000

    fixup for redis and postgres dbs env

--- complex/worker/index.js
+++ complex/worker/index.js
@@ -9,6 +9,7 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
+  console.log('Calculating Fib number for index: $index');
   if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
 }
